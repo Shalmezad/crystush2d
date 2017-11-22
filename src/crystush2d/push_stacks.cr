ad01ee527a0f24c01065670f4c6f9889b886ae98
Richard Wardin <richardw@hesonline.com>
2017-11-21 20:07:13 -0500

    Fixed issue with to_s

--- src/crystush2d/push_stacks.cr
+++ src/crystush2d/push_stacks.cr
@@ -1,11 +1,10 @@
 require "crystush"
 module Crystush2d
   class PushStacks < Crystush::PushStacks
-    # TODO: Make array of Point[s]
     property point_stack : Array(Point) = [] of Point
 
     def to_s(io)
-      super.to_s(io)
+      super(io)
       io << "POINT  : " << point_stack << "\n"
     end
   end
